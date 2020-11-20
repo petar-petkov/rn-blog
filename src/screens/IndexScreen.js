047166e2f3a0a7ff6809f05e02c728212c899261
Petar Petkov <pgpetkov@protonmail.com>
2020-11-20 13:09:40 +0200

    Add context provider to app

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const IndexScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default IndexScreen;
