1e05252fbd73c8cc259b3b6de7054532f481219e
Petar Petkov <pgpetkov@protonmail.com>
2020-11-20 18:22:54 +0200

    Refactor blog provider to use reducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext = React.createContext();
 
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blog_post':
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
 
+export const BlogProvider = ({ children }) => {
+  // useReducer -> Reducer - Initial State
+  const [payload, dispatch] = useReducer(blogReducer, []);
+  
   const addBlogPost = () => {
-    setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }]);
+    dispatch({ type: 'add_blog_post' });
   };
 
   // {children} represents for example <App />
   return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>
+    <BlogContext.Provider value={{ data: payload, addBlogPost }}>
       {children}
     </BlogContext.Provider>
   );
